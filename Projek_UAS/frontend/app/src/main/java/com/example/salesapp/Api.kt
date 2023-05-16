de6f6e71f42b405018fa89bb05c43c3015dfd7c9
Stefannus Christian <schristian38@students.calvin.ac.id>
2023-05-16 20:43:59 +0700

    delete unnecessary files

--- Projek_UAS/frontend/app/src/main/java/com/example/salesapp/Api.kt
+++ Projek_UAS/frontend/app/src/main/java/com/example/salesapp/Api.kt
@@ -4,9 +4,5 @@ import retrofit2.Call
 import retrofit2.http.*
 
 interface Api {
-    //    Get All Products
-//    @GET("getallproducts")
-//    fun getAllProducts(): Call<ArrayList<GetProductResponse>>
-    @GET("getallproducts")
-    fun getAllProducts(): Call<ArrayList<GetProductResponse>>
+
 }
